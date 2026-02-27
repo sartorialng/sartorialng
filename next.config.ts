5241b16d0191c82b49ed809cb32f848c18cca61d
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2026-02-27 09:13:05 +0100

    remove csp

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.snapchat.com https://*.sc-static.net;
-    connect-src 'self' https://*.snapchat.com https://*.sc-static.net;
-    img-src 'self' https://*.snapchat.com https://*.sc-cdn.net;
-    frame-src 'self' https://*.snapchat.com;
-    style-src 'self' 'unsafe-inline';
-`
-	.replace(/\s{2,}/g, " ")
-	.trim();
-
 const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
@@ -41,19 +30,6 @@ const nextConfig: NextConfig = {
 		fetches: {
 			fullUrl: true,
 		},
-	},
-	async headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: [
-					{
-						key: "Content-Security-Policy",
-						value: cspHeader,
-					},
-				],
-			},
-		];
 	},
 };
 
