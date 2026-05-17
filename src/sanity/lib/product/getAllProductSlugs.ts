100d4077a5eb8e46932935df33ac408f9f85eddb
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2026-05-17 22:14:05 +0100

    optimize product details page

---
+++ src/sanity/lib/product/getAllProductSlugs.ts
@@ -0,0 +1,7 @@
+import { client } from "../client";
+import { groq } from "next-sanity";
+
+export const getAllProductSlugs = async (): Promise<{ slug: string }[]> => {
+	const query = groq`*[_type == "product" && defined(slug.current)]{ "slug": slug.current }`;
+	return client.fetch(query, {}, { next: { revalidate: 3600 } });
+};
